ebc4778c30f507d192924e72c6e720585d064161
Caleb Calderwood <calderwoodc@gmail.com>
2024-01-18 11:43:03 -0700

    bishop_move_calc

--- shared/src/main/java/chess/piece_moves/BishopMovesCalculator.java
+++ shared/src/main/java/chess/piece_moves/BishopMovesCalculator.java
@@ -1,5 +1,64 @@
 package chess.piece_moves;
 
-public class BishopMovesCalculator {
-    
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+import java.util.Collection;
+public class BishopMovesCalculator extends PieceMovesCalculator{
+
+    public BishopMovesCalculator(ChessBoard board, ChessPosition position){
+        super(board, position);
+    }
+    public Collection<ChessMove> Legal_Moves_Calc(){
+        int start_row = position.getRow();
+        int start_col = position.getColumn();
+        ArrayList<ChessMove> legal_moves = new ArrayList<ChessMove>();
+        for (int i = start_row+1; i < 8; i++) { //up-right diagonal
+            int j = start_col;
+            j++;
+            ChessPosition end_pos = new ChessPosition(i, j);
+            ChessMove possible_move = new ChessMove(position, end_pos, null);
+            if (board.getPiece(end_pos)!= null){
+                break;
+            }
+            legal_moves.add(possible_move);
+        }
+
+        for (int i = start_row-1; i > 0; i--) { //up-left diagonal
+            int j = start_col;
+            j++;
+            ChessPosition end_pos = new ChessPosition(i, j);
+            ChessMove possible_move = new ChessMove(position, end_pos, null);
+            if (board.getPiece(end_pos)!= null){
+                break;
+            }
+            legal_moves.add(possible_move);
+        }
+
+        for (int i = start_row+1; i < 8; i++) { //down-right diagonal
+            int j = start_col;
+            j--;
+            ChessPosition end_pos = new ChessPosition(i, j);
+            ChessMove possible_move = new ChessMove(position, end_pos, null);
+            if (board.getPiece(end_pos)!= null){
+                break;
+            }
+            legal_moves.add(possible_move);
+        }
+
+        for (int i = start_row-1; i > 0; i--) { //down-left diagonal
+            int j = start_col;
+            j--;
+            ChessPosition end_pos = new ChessPosition(i, j);
+            ChessMove possible_move = new ChessMove(position, end_pos, null);
+            if (board.getPiece(end_pos)!= null){
+                break;
+            }
+            legal_moves.add(possible_move);
+        }
+
+        return  legal_moves;
+    }
 }
--- shared/src/main/java/chess/piece_moves/PieceMovesCalculator.java
+++ shared/src/main/java/chess/piece_moves/PieceMovesCalculator.java
@@ -5,7 +5,16 @@ import chess.ChessPosition;
 
 public class PieceMovesCalculator {
 
-    public PieceMovesCalculator(ChessBoard board , ChessPosition position){
+    protected final ChessBoard board;
+    protected final ChessPosition position;
 
+    public PieceMovesCalculator(ChessBoard board, ChessPosition position){
+        this.board = board;
+        this.position = position;
     }
+
+//    public PieceMovesCalculator() {
+//        this.board = new ChessBoard();
+//        this.position = new ChessPosition(0, 0);
+//    }
 }
