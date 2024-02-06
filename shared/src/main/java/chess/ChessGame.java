5c0e8ca4ce37c86bf19db7c51e269380db4f52ef
Caleb Calderwood <calderwoodc@gmail.com>
2024-02-05 17:16:02 -0700

    added removePiece function

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -56,6 +56,7 @@ public class ChessGame {
         }
         ChessBoard sim_board = new ChessBoard(board);
         for(ChessMove move: piece.pieceMoves(board, startPosition)){
+            sim_board.addPiece(move.getEndPosition(), piece);
 
         }
 
