7a8c658483515c4107075fea7b94feef21d69cab
Caleb Calderwood <calderwoodc@gmail.com>
2024-02-21 19:25:36 -0700

    added services

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class GameService {
+//    public Collection<GameData> listGames(UserData user) {}
+//    public GameData createGame(UserData user) {}
+//    public void joinGame(UserData user) {}
+}
