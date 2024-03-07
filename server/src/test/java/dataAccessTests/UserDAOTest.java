4f221d5ee954d5e8e95ad74a4af61139bd501fb8
Caleb Calderwood <calderwoodc@gmail.com>
2024-03-07 11:34:08 -0700

    made some userDAO tests

--- server/src/test/java/dataAccessTests/UserDAOTest.java
+++ server/src/test/java/dataAccessTests/UserDAOTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.ClearService;
 
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserDAOTest {
@@ -15,7 +17,60 @@ public class UserDAOTest {
     public SqlUserDAO userDatabase;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws DataAccessException {
         this.userDatabase = new SqlUserDAO();
+        userDatabase.clear();
+    }
+
+    @Test
+    public void testCreateUser()  throws DataAccessException{
+        UserData user = new UserData("testName", "testPass", "test@gmail.com");
+        userDatabase.createUser(user);
+        UserData dbUser = userDatabase.getUser(user.username());
+        assertEquals(user.username(), dbUser.username());
+    }
+
+    @Test
+    public void failCreateUser()  throws DataAccessException{
+        UserData user = new UserData(null, "testPass", "test@gmail.com");
+
+        assertThrows(DataAccessException.class, () -> {
+            userDatabase.createUser(user);
+        });
+    }
+
+    @Test
+    public void testGetUser()  throws DataAccessException{
+
+    }
+
+    @Test
+    public void failGetUser()  throws DataAccessException{
+
+    }
+
+    @Test
+    public void testListUsers()  throws DataAccessException{
+
+    }
+
+    @Test
+    public void failListUsers()  throws DataAccessException{
+
+    }
+
+    @Test
+    public void testUserExists() throws DataAccessException{
+
+    }
+
+    @Test
+    public void failUserExists() throws DataAccessException{
+
+    }
+
+    @Test
+    public void testClear()  throws DataAccessException{
+
     }
 }
