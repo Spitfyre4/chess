d71e11dc75078f3e1d1440ddddf3fab083b4a7d6
Caleb Calderwood <calderwoodc@gmail.com>
2024-03-07 12:06:30 -0700

    finished gameDAO tests

--- server/src/test/java/dataAccessTests/GameDAOTest.java
+++ server/src/test/java/dataAccessTests/GameDAOTest.java
@@ -4,13 +4,14 @@ import chess.ChessGame;
 import dataAccess.DataAccessException;
 import dataAccess.SqlGameDAO;
 import model.GameData;
+import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameDAOTest {
 
@@ -27,7 +28,9 @@ public class GameDAOTest {
         GameData game = new GameData(1, null, null, "game1", new ChessGame());
         gameDatabase.createGame(game);
 
-        assertEquals(game.gameName(), gameDatabase.getGame(1).gameName());
+        ArrayList<GameData> gameList = new ArrayList<>(gameDatabase.listGames());
+
+        assertEquals(1, gameList.size());
     }
 
     @Test
@@ -42,36 +45,72 @@ public class GameDAOTest {
 
     @Test
     public void testGetGame() throws DataAccessException{
+        GameData game = new GameData(1, null, null, "game1", new ChessGame());
+        gameDatabase.createGame(game);
 
+        assertEquals(game.gameName(), gameDatabase.getGame(1).gameName());
     }
 
     @Test
     public void failGetGame() throws DataAccessException{
+        GameData game = new GameData(1, null, null, "game1", new ChessGame());
 
+        assertThrows(DataAccessException.class, () -> {
+            gameDatabase.getGame(game.gameID());
+        });
     }
 
     @Test
     public void testListGames() throws DataAccessException{
+        GameData game = new GameData(1, null, null, "game1", new ChessGame());
+        GameData game2 = new GameData(2, null, null, "game2", new ChessGame());
+        gameDatabase.createGame(game);
+        gameDatabase.createGame(game2);
 
+        ArrayList<GameData> gameList = new ArrayList<>(gameDatabase.listGames());
+
+        assertEquals(2, gameList.size());
     }
 
     @Test
     public void failListGames() throws DataAccessException{
+        GameData game = new GameData(1, null, null, "game1", new ChessGame());
+        GameData game2 = new GameData(2, null, null, "game2", new ChessGame());
+        gameDatabase.createGame(game);
+        gameDatabase.createGame(game2);
 
+        ArrayList<GameData> gameList = new ArrayList<>(gameDatabase.listGames());
+
+        assertNotEquals(1, gameList.size());
     }
 
     @Test
     public void testJoinGame() throws DataAccessException{
+        GameData game = new GameData(1, null, null, "game1", new ChessGame());
+        gameDatabase.createGame(game);
+
+        gameDatabase.joinGame("username1", "WHITE", 1);
+
+        assertEquals("username1", gameDatabase.getGame(1).whiteUsername());
 
     }
 
     @Test
     public void failJoinGame() throws DataAccessException{
+        GameData game = new GameData(1, null, null, "game1", new ChessGame());
+        gameDatabase.createGame(game);
 
+        assertThrows(DataAccessException.class, () -> {
+            gameDatabase.joinGame("username1", "WHITE", 2);
+        });
     }
 
     @Test
     public void testClear() throws DataAccessException{
+        gameDatabase.clear();
 
+        ArrayList<GameData> gameList = new ArrayList<>(gameDatabase.listGames());
+
+        assertEquals(0, gameList.size());
     }
 }
