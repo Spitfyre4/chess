f5e14ebafd50b893446206ec5a2ba993611865d5
Caleb Calderwood <calderwoodc@gmail.com>
2024-04-05 15:10:41 -0600

    added some more websocket classes

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,4 +1,23 @@
 package server.websocket;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import webSocketMessages.serverMessages.ServerMessage;
+
+import java.io.IOException;
+
 public class WebSocketHandler {
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        ServerMessage sMessage = new Gson().fromJson(message, ServerMessage.class);
+        switch (sMessage.getServerMessageType()) {
+            case LOAD_GAME -> loadGame();
+            case ERROR -> error();
+            case NOTIFICATION -> notifaction();
+        }
+    }
 }
