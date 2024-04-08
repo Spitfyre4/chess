eca8da6f68ba4bee3e92134b02394fe5200ee73f
Caleb Calderwood <calderwoodc@gmail.com>
2024-04-08 15:47:15 -0600

    fixed connection and connection manager to take username, auth, and gameID

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -1,16 +1,21 @@
 package server.websocket;
 
+import model.GameID;
 import org.eclipse.jetty.websocket.api.Session;
 
 import java.io.IOException;
 
 public class Connection {
-    public String visitorName;
+    public String username;
     public Session session;
+    public String authToken;
+    public GameID gameID;
 
-    public Connection(String visitorName, Session session) {
-        this.visitorName = visitorName;
+    public Connection(String username, String authToken, GameID gameID, Session session) {
+        this.username = username;
         this.session = session;
+        this.authToken = authToken;
+        this.gameID = gameID;
     }
 
     public void send(String msg) throws IOException {
--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,7 +1,8 @@
 package server.websocket;
 
+import model.GameID;
 import org.eclipse.jetty.websocket.api.Session;
-import webSocketMessages.Notification;
+import webSocketMessages.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,21 +11,21 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionManager {
     public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
 
-    public void add(String visitorName, Session session) {
-        var connection = new Connection(visitorName, session);
-        connections.put(visitorName, connection);
+    public void add(String username, String authToken, GameID gameID, Session session) {
+        var connection = new Connection(username, authToken, gameID, session);
+        connections.put(username, connection);
     }
 
-    public void remove(String visitorName) {
-        connections.remove(visitorName);
+    public void remove(String username) {
+        connections.remove(username);
     }
 
-    public void broadcast(String excludeVisitorName, Notification notification) throws IOException {
+    public void broadcast(String excludeAuth, String message, GameID gameID) throws IOException {
         var removeList = new ArrayList<Connection>();
         for (var c : connections.values()) {
             if (c.session.isOpen()) {
-                if (!c.visitorName.equals(excludeVisitorName)) {
-                    c.send(notification.toString());
+                if (!c.username.equals(excludeAuth)) {
+                    c.send(message);
                 }
             } else {
                 removeList.add(c);
@@ -33,7 +34,7 @@ public class ConnectionManager {
 
         // Clean up any connections that were left open.
         for (var c : removeList) {
-            connections.remove(c.visitorName);
+            connections.remove(c.username);
         }
     }
 }
